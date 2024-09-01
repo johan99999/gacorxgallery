9822e35602af1fa5aa2cf44406de42ee743ae478
hanlie <johansama9999@gmail.com>
2024-09-02 01:18:51 +0700

    [UPDATE] Memberikan pengaturan agar gambar tidak bisa diakses ketika di klik kanan

--- js/script.js
+++ js/script.js
@@ -7,3 +7,23 @@ document.querySelectorAll('.gallery img').forEach(function(img) {
         this.style.transform = 'scale(1)';
     });
 });
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === "PrintScreen") {
+        // Hide content or show a black overlay
+        document.body.style.visibility = 'hidden';
+    }
+});
+
+document.addEventListener('keyup', function(e) {
+    if (e.key == "PrintScreen") {
+        navigator.clipboard.writeText('');
+        alert('Screenshots are disabled.');
+    }
+});
+
+document.addEventListener('fullscreenchange', function() {
+    if (document.fullscreenElement) {
+        alert('Screenshots and screen recordings are not allowed.');
+    }
+});
